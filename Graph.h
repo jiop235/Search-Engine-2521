d1b758defe128866bc8ad3b85899cd927cafb738
jiop235 <31943929+jiop235@users.noreply.github.com>
2018-10-16 22:51:32 +1100

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -2,9 +2,20 @@
 //General structure attributed to week9 2521 lab "Graph.h" written by John Shepard, March 2013
 
 #include <stdio.h>
+#include "List.h"
 
 //Graph Struct
 typedef struct GraphRep *Graph;
+
+typedef struct GraphRep{
+	int nV;			//# vertuces
+	int nE;
+	//int maxV;		//Max vertexs
+	//int **edges;	//matrix of weights (0 == no edges)
+	List *edges;
+	char **index_URL;	//Array of urls to there index
+} GraphRep;
+
 Graph createGraph(int nV);
 void destroyGraph(Graph);
 
@@ -13,8 +24,8 @@ typedef int Vertex;
 
 //Edges = link between 2 vertices
 typedef struct { Vertex v; Vertex w;} Edge; 
-void insertEdge(Graph, Vertex, Vertex, int);
-void removeEdge(Graph, Vertex, Vertex);
+void insertEdge(Graph, Vertex, Vertex);//, int);
+//
 
 int isConnected(Graph, Vertex, Vertex);
 void showGraph(Graph);
