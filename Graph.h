1e9faf17dce6e4031d30acd2f587964dd6fc531b
jiop235 <31943929+jiop235@users.noreply.github.com>
2018-10-17 22:38:28 +1100

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -1,8 +1,13 @@
 //Graph.h ADT
 //General structure attributed to week9 2521 lab "Graph.h" written by John Shepard, March 2013
 
+#ifndef GRAPH_H
+#define GRAPH_H
+
 #include <stdio.h>
 #include "List.h"
+
+
 
 //Graph Struct
 typedef struct GraphRep *Graph;
@@ -23,9 +28,11 @@ void destroyGraph(Graph);
 typedef int Vertex;
 
 //Edges = link between 2 vertices
-typedef struct { Vertex v; Vertex w;} Edge; 
+//typedef struct { Vertex v; Vertex w;} Edge; 
 void insertEdge(Graph, Vertex, Vertex);//, int);
 //
 
 int isConnected(Graph, Vertex, Vertex);
 void showGraph(Graph);
+
+#endif
