ea6e88118c8e82535cecbdfe804d57c2785c9f45
jiop235 <31943929+jiop235@users.noreply.github.com>
2018-10-17 22:37:23 +1100

    Update readData.h

--- readData.h
+++ readData.h
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include "Graph.h"
+#include "List.h"
 
-Graph readCollection(char* collection);
+Graph readCollection(char*);
+
+//insert URL into graph
+void insert_URLS(Graph g);
 int URL_to_index(char *index_URL[], char *URL);
 char* index_to_URL(char *index_URL[], int index);
