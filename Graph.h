be65d93935b38d6006a3152f9091d009a5e4c233
jiop235 <31943929+jiop235@users.noreply.github.com>
2018-10-20 20:43:58 +1100

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -32,6 +32,7 @@ typedef int Vertex;
 void insertEdge(Graph, Vertex, Vertex);//, int);
 //
 
+void getWeightedValues(List L, Graph g);
 float pageRankCalc(List L, Graph);
 
 int isConnected(Graph, Vertex, Vertex);
