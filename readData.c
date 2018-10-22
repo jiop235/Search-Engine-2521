cc3e5c383e50d44d91647a3c61aa8b9b12c09674
jiop235 <31943929+jiop235@users.noreply.github.com>
2018-10-22 12:06:24 +1100

    Update readData.c

--- readData.c
+++ readData.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-#include "readData.h"
 #include "Graph.h"
 #include "List.h"
+#include "readData.h"
 
 #define FALSE 0
 #define TRUE 1
@@ -14,66 +14,62 @@
 Graph readCollection(char* collection){
 	FILE *fp;
 	Graph pageGraph;
-	fp = fopen(collection, "r");
+	if((fp = fopen(collection, "r")) == NULL) return NULL;
 
 	//Read Urls
 	char curr_URL[URL_SIZE]; // Dynamically allocate this shit
-	int url_count = 0; // Counts the nV / URLS that are in the collection.txt
+	int url_count = 0; 		 // Counts the nV / URLS that are in the collection.txt
 
-	//List webSites;
-
-	//Determine size of graph
+	//Determine size of graph and initialise
 	while(fscanf(fp, "%s", curr_URL) != EOF){	//Find some way to read each WORD
 		printf("%d) testing %s\n", url_count, curr_URL);
 		url_count++;
 	}
-
-	//HOLDS THE INDEX value to the URL string so (e.g. 0 = url23, 1 = url56 etc)
-	//char *index_URL[url_count - 1];	
 	pageGraph = createGraph(url_count);
+
+	//g->index_URL[] - HOLDS THE INDEX value to the URL string so (e.g. 0 = url23, 1 = url56 etc)
+
+	//Load URLS into the g->index_URLS[]
 	rewind(fp); 							//Rewinds the fp to start of pointer
 	url_count = 0;
 	while(fscanf(fp, "%s", curr_URL) != EOF){
 		pageGraph->index_URL[url_count] = strdup(curr_URL);
 		url_count++;
 	}
-	/*int i = 0;
-	for(i = 0; i < url_count; i++) printf("%d) %s\n", i, pageGraph->index_URL[i]);*/
+
 	insert_URLS(pageGraph);
-	printf("hello?\n");
 	fclose(fp);
 
 	return pageGraph;
 }
 
-void insert_URLS(Graph g){
-	int i = 0;
-	printf("%d\n", g->nV);
 
-	while(i < g->nV){
-		//printf("hehe\n");
+void insert_URLS(Graph g){
+	int i;
+
+	for(i = 0; i < g->nV; i++){
+		insertIndex(g->edges[i], i);
 		FILE *fp;
-		//Add .txt at end of string;
+		//Add .txt at end of string
 		char *curr_URL = malloc(sizeof(g->index_URL[i]) + 5);
 		strcpy(curr_URL, g->index_URL[i]);
 		strcat(curr_URL, ".txt");
 
 		fp = fopen(curr_URL, "r");
-		if(fp == NULL) printf("yikes\n");
+		if(fp == NULL){ printf("URL.txt does not exist\n"); break;}
 		char curr_string[URL_SIZE];
+
 		while(fscanf(fp, "%s", curr_string) != EOF){
-			printf("%s\n", curr_string);
-			if(strcmp(curr_string, "#end") == 0){ break;	}
+			if(strcmp(curr_string, "#end") == 0){ break; }
 			else if((strcmp(curr_string,"#start") == 0) || (strcmp(curr_string,"Section-1") == 0)){
 				continue;
 			}else{
 				insertEdge(g, i, URL_to_index(g->index_URL, curr_string));
 			}
 		}
-		printf("HIII??? %d\n", g->nV);
+
 		fclose(fp);
 		free(curr_URL);
-		i++;
 	}
 }
 
