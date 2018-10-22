9fc528eb6a08f9a3f0b9bf212b9c8ff83326c7f0
jiop235 <31943929+jiop235@users.noreply.github.com>
2018-10-22 12:04:23 +1100

    Update pagerank.c

--- pagerank.c
+++ pagerank.c
@@ -17,68 +17,16 @@
 
 
 void totalPageRank (Graph g, float d, float diffPR, int maxIterations);
+void createPageList(Graph g);
 
 int main(){
-
 
 	Graph pageGraph = readCollection("collection.txt");
 	totalPageRank(pageGraph, 0.85, 0.00001, 10000);
 	showGraph(pageGraph);
-	
-	int i = 0, j;
-	FILE *fp;
-	fp = fopen("pagerankList.txt", "w");
-
-	int visited[pageGraph->nV];
-	for(i = 0; i < pageGraph->nV; i++) visited[i] = 0;
-	float highestPageRank = 0;
-	int index;
-	for(j = 0; j < pageGraph->nV; j++){
-		for(i = 0; i < pageGraph->nV; i++){
-			if(visited[i] == 1) continue;
-			if(getPageRank(pageGraph->edges[i]) > highestPageRank){
-				highestPageRank = getPageRank(pageGraph->edges[i]);
-				index = i;
-			} 
-		}
-		fprintf(fp, "%s, %d, %.7f\n", pageGraph->index_URL[index], 
-		getOutLinks(pageGraph->edges[index]), getPageRank(pageGraph->edges[index]) );
-		visited[index] = 1;
-		highestPageRank = 0;
-	}
-
-	fclose(fp);
+	createPageList(pageGraph);
 	destroyGraph(pageGraph);
-	
 	return 0;
-	/*
-	FILE *fp;
-	Graph pageGraph;
-	fp = fopen("collection.txt", "r");
-
-	//Read Urls
-	char curr_URL[URL_SIZE]; // Dynamically allocate this shit
-	int url_count = 0; // Counts the nV / URLS that are in the collection.txt
-
-	//List webSites;
-
-	//Determine size of graph
-	while(fscanf(fp, "%s", curr_URL) != EOF){	//Find some way to read each WORD
-		printf("%d) testing %s\n", url_count, curr_URL);
-		url_count++;
-	}
-	char *index_URL[url_count - 1];	//Dynamically allocate this shit, HOLDS THE INDEX value to the URL string so (e.g. 0 = url23, 1 = url56 etc)
-	pageGraph = createGraph(url_count);
-	rewind(fp); 							//Rewinds the fp to start of pointer
-	url_count = 0;
-	while(fscanf(fp, "%s", curr_URL) != EOF){
-		index_URL[url_count] = strdup(curr_URL);
-		url_count++;
-	}*/
-
-
-
-	//Add values into it
 
 }
 
@@ -94,41 +42,60 @@ void totalPageRank (Graph g, float d, float diffPR, int maxIterations){
 		newPageRank[i] = 0;
 	}
 
-
-	//float inP;
 	int interation = 0;
 	float diff = diffPR;
-	//float prevPageRank;
 	float sumCurrDiff = 0;
 
+	//Set the weighted values
 	for(i = 0; i < g->nV; i++){
 		getWeightedValues(g->edges[i], g);
 	}
 
 	while(interation < maxIterations && diff >= diffPR){
-		//printf("interation %d\n", interation);
+		//Get the new page ranks for the next interation
+		//pageRankCalc calculation summation part of equation.
 		for(i = 0; i < g->nV; i++){
-			//prevPageRank = getPageRank(g->edges[i]);
-			//printf("prevPageRank %f\n", prevPageRank);
-			
-			printf("CURRENT URL %s\n", g->index_URL[i]);
 			newPageRank[i] = ((1 - d)/N) + (d * pageRankCalc(g->edges[i], g));
-			//rintf("newPageRank %f\n", newPageRank[i]);
-			//changePageRank(g->edges[i], ((1 - d)/N) + (d * pageRankCalc(g->edges[i], g)));
-
-			//printf("after equation %f\n", getPageRank(g->edges[i]));
 		}
 		for(i = 0; i < g->nV; i++){
-			//printf("sumCurrDiff %f %f \n", sumCurrDiff, fabsf(getPageRank(g->edges[i]) - prevPageRank));
 			sumCurrDiff += fabsf((newPageRank[i] - getPageRank(g->edges[i])));
-			//printf("sumCurrDiff %f\n", sumCurrDiff);
 			changePageRank(g->edges[i], newPageRank[i]);
 		}
 		diff = sumCurrDiff;
-		//printf("diff %f\n", diff);
 		sumCurrDiff = 0;
 		interation++;
 	}
 	
 }
 
+void createPageList(Graph g){
+
+	int i, j;
+	float highestPageRank = 0;
+	int index;		
+	int visited[g->nV];
+	//Set visited to 0
+	for(i = 0; i < g->nV; i++) visited[i] = 0;
+	
+	FILE *fp;
+	fp = fopen("pagerankList.txt", "w");
+	if(fp == NULL) return;
+	
+
+	//Insertion Sort sorting
+	for(j = 0; j < g->nV; j++){
+		for(i = 0; i < g->nV; i++){
+			if(visited[i] == 1) continue;
+			if(getPageRank(g->edges[i]) > highestPageRank){
+				highestPageRank = getPageRank(g->edges[i]);
+				index = i;
+			} 
+		}
+		fprintf(fp, "%s, %d, %.7f\n", g->index_URL[index], 
+		getOutLinks(g->edges[index]), getPageRank(g->edges[index]) );
+		visited[index] = 1;
+		highestPageRank = 0;
+	}
+
+	fclose(fp);
+}
